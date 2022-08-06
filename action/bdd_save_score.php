e5039bf57794beff9a180a310bd9111445837b64
Reoddyn <59644898+Reoddyn@users.noreply.github.com>
2022-08-06 22:34:12 +0200

    Create bdd_save_score.php

---
+++ action/bdd_save_score.php
@@ -0,0 +1,67 @@
+<?php
+set_error_handler(function($errno, $errstr, $errfile, $errline, $errcontext) {
+	/* error was suppressed with the @-operator */
+	if (0 === error_reporting()) {
+		return false;
+	}
+	throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
+});
+
+try{
+	if (file_exists('../param/global.php')) // vérif nécessaire pour les appels ajax
+	{
+		$p = "../";	
+	}
+	else
+	{
+		$p = ".../";		
+	}
+	include_once($p.'param/global.php');
+	include_once($p.'fonctions/php_cookies.php');
+	$data = [];
+	$data["erreur"] = false;
+	
+	
+	/********* JUST GET THIS SPECIFIC CODE OUT OF HERE ****************/
+	$servername = "localhost";
+	$username = "bourget";
+	$password = "test";
+	$dbname = "bourget";
+	
+	$sql = "";
+	$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	
+	if (cookieExists(COOKIE_MISSIONS)) {
+		if (isset($_POST['team'])) {
+			// formulaire envoyé : insérer la team dans la bdd !
+			// INSERT INTO `utilisateur` (`nom`, `prenom`, `email`) VALUES ('Durantay', 'Quentin', 'quentin@gmail.com');
+			$array = ["nom" => $_POST['team'], "start" => $_POST['start'], "end" => $_POST['end'], "rep" => $_POST['rep_juste'], "duree" => $_POST['duree']];
+			$sql = "INSERT INTO `game` (`nom`, `start`, `end`, `reponsejuste`, `duree`) VALUES (:nom, :start, :end, :rep, :duree)";
+		}
+		else {
+			throw new Exception("Le formulaire n'a pas été envoyé correctement.");
+		}
+	} else {
+		throw new Exception("Affichage impossible. Erreur cookie. Je crains que vous ne deviez <a href=\"./\">recommencer le jeu...</a>");
+	}
+	
+}
+catch(PDOException $e){
+	$data["erreur"] = true;
+	$data["msg"] = "<p>Erreur auprès de la base de données.</p><p>Requête : $sql</p><p>".$e->getMessage()."</p>";
+}
+catch(Exception $e){
+	$data["erreur"] = true;
+	$data["msg"] = "<p>Une exception est survenue. Merci de contacter un administrateur.</p><p>".$e->getMessage()."</p>";
+}
+catch(Error $e){
+	$data["erreur"] = true;
+	$data["msg"] = "<p>Une erreur est survenue. Merci de contacter un administrateur.</p><p>".$e->getMessage()."</p>";
+}
+finally{
+	echo json_encode($data);
+	restore_error_handler();
+}
+
+?>
