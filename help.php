4ebe4848b9b7ff61f175706342a9b9026b69b02f
LudoConcept <lmarechal.dev@gmail.com>
2022-08-11 17:42:28 +0200

    TODO Faire marcher le bouton qui mène à la mission 13

--- help.php
+++ help.php
@@ -29,6 +29,8 @@ echo ("<br>Valeur mission ROSES<br><br>Avant changement : ".$read_val_cook_ROSES
 <br><br>
 <input type=button onclick=window.location.href='https://jeuext.la-grande-evasion.com/bourget/index.php'; value='Retourner à la Game'/>
 <br><br><br><br><br>Si ça n'a pas marché !<br><br>
-<input type=button onclick=window.location.href='https://jeuext.la-grande-evasion.com/bourget/index.php'; value='Forcer la mission ROSES'/>
+<a class="cercle point_actif" style="top: 41%; left: 30%; width:23%; height:2%;" href="https://jeuext.la-grande-evasion.com/bourget/interface.php" title="13"></a>
+<br><br><br><br><br><br>
+<input type=button onclick=window.location.href='https://jeuext.la-grande-evasion.com/bourget/interface.php'; value='Forcer la mission ROSES'/>
 </body>
 </html>
