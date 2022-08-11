a2c5259ae70ae94e0cb2a9c9c83fe20556be5329
LudoConcept <lmarechal.dev@gmail.com>
2022-08-11 17:36:14 +0200

    Création du fichier help.php
    Pour nous sauver la vie en cas d'urgence !

---
+++ help.php
@@ -0,0 +1,34 @@
+
+<?php
+	include_once('./param/global.php');
+	include_once('./fonctions/php_cookies.php');
+	include_once('./fonctions/php_mission.php');
+	include_once('./textes/textes.php');
+
+	if (cookieExists(COOKIE_MISSIONS)) {
+		$etatmissions = unserialize($_COOKIE[COOKIE_MISSIONS]);
+		$read_val_cook_ROSES = $etatmissions[13];
+		$etatmissions[13] = ACTIVE;
+		createCookie(COOKIE_MISSIONS, serialize($etatmissions));
+	}
+?>
+<html>
+<head>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<link href="<?php echo FICHIER_CSS; ?>" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<script src="./js/Cook.js"></script>
+<script>
+
+</script>
+<?php
+echo ("<br>Valeur mission ROSES<br><br>Avant changement : ".$read_val_cook_ROSES."<br>Après changement : ".ACTIVE);
+?>
+<br><br>
+<input type=button onclick=window.location.href='https://jeuext.la-grande-evasion.com/bourget/index.php'; value='Retourner à la Game'/>
+<br><br><br><br><br>Si ça n'a pas marché !<br><br>
+<input type=button onclick=window.location.href='https://jeuext.la-grande-evasion.com/bourget/index.php'; value='Forcer la mission ROSES'/>
+</body>
+</html>
