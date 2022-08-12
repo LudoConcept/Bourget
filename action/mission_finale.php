41d38a9d301b35c19a339be11a2470d0a1f3e38e
Reoddyn <59644898+Reoddyn@users.noreply.github.com>
2022-08-12 21:19:22 +0200

    Update mission_finale.php

--- action/mission_finale.php
+++ action/mission_finale.php
@@ -121,6 +121,7 @@ $save_temps_en_sec = $temps_en_sec;
 <form>
 	<!-- <?php echo $temps_depart; ?> <?php echo $temps_fin; ?> <?php echo $duree; ?> <?php echo $nb_juste; ?> -->
 	<!-- data à sauver pour les teams + manque nom de la team -->
+	<input type="text" name="team" value="" placeholder="Votre nom d'équipe"><input type="submit" value="Enregistrer votre temps !" id="saveteam">
 	<input type="hidden" name="start" value="<?php echo $temps_depart; ?>">
 	<input type="hidden" name="end" value="<?php echo $temps_fin; ?>">
 	<input type="hidden" name="duree" value="<?php echo $duree; ?>">
