c00ff1db71004d798624fb223553af77fc7f2db0
LudoConcept <lmarechal.dev@gmail.com>
2022-08-12 22:31:36 +0200

    Update bdd_save_score.php

--- action/bdd_save_score.php
+++ action/bdd_save_score.php
@@ -18,23 +18,29 @@ try{
 	}
 	include_once($p.'param/global.php');
 	include_once($p.'fonctions/php_cookies.php');
+
+	include_once($p.'action/bdd_connect.php');
+	
+	// check des conditions + prépa des valeurs de retour
 	$data = [];
 	$data["erreur"] = false;
 	
-	include_once($p.'action/bdd_connect.php');
+	if (!cookieExists(COOKIE_MISSIONS)) {
+		$data['erreur'] = true;
+		throw new Exception("Erreur, aucune donnée dans les cookies. Je crains que vous ne deviez <a href=\"./\">recommencer le jeu...</a>");
+	}
+	if (!isset($_POST['team']) && !isset($_POST['start']) && !isset($_POST['end']) && !isset($_POST['duree']) && !isset($_POST['rep_juste']) && is_numeric($_POST['start']) && is_numeric($_POST['end']) && is_numeric($_POST['duree']) && is_numeric($_POST['rep_juste'])) {
+		$data['erreur'] = true;
+		throw new Exception("Erreur, aucune donnée envoyées via le formulaire. ");
+	}
 	
-	if (cookieExists(COOKIE_MISSIONS)) {
+	if (!data['erreur']) {
 		if (isset($_POST['team'])) {
 			// formulaire envoyé : insérer la team dans la bdd !
 			// INSERT INTO `utilisateur` (`nom`, `prenom`, `email`) VALUES ('Durantay', 'Quentin', 'quentin@gmail.com');
 			$array = ["nom" => $_POST['team'], "start" => $_POST['start'], "end" => $_POST['end'], "rep" => $_POST['rep_juste'], "duree" => $_POST['duree']];
 			$sql = "INSERT INTO `game` (`nom`, `start`, `end`, `reponsejuste`, `duree`) VALUES (:nom, :start, :end, :rep, :duree)";
 		}
-		else {
-			throw new Exception("Le formulaire n'a pas été envoyé correctement.");
-		}
-	} else {
-		throw new Exception("Affichage impossible. Erreur cookie. Je crains que vous ne deviez <a href=\"./\">recommencer le jeu...</a>");
 	}
 	
 }
