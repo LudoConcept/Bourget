9e5884f5708b3201e9c2a41a7e1a667b57fca62f
Reoddyn <59644898+Reoddyn@users.noreply.github.com>
2022-08-13 13:53:38 +0200

    Update endgame.js

--- js/endgame.js
+++ js/endgame.js
@@ -3,6 +3,22 @@ $(function(){
 		event.preventDefault();
 		console.log("save team");
 		// tenter d'ajouter la team dans la base
+		console.log($("#saveteamform").serialize());
+		
+		$.post('./action/bdd_save_score.php',
+			$("#saveteamform").serialize(),
+			function(result){
+				console.log("done");
+				console.log(result);
+			}
+		).fail(function(erreur){
+			console.log("fail");
+			console.log(erreur);
+		}).always(function(res){
+			console.log("always");
+			console.log(res);
+		});
+		
 		// traiter la réponse : la team n'est pas ajouté dans la base ----> on reste et on affiche un petit mot
 		// traiter la réponse : team enregistrée ----> on chrge la suite, trois étapes pour afficher les scores.
 	});
