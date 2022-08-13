8fb66d7cb4a722cb68db3f30d697a06ef2bd5b79
Reoddyn <59644898+Reoddyn@users.noreply.github.com>
2022-08-13 14:18:15 +0200

    Update endgame.js

--- js/endgame.js
+++ js/endgame.js
@@ -1,5 +1,7 @@
+var blou;
 $(function(){
 	$("#bloc_mission").on("click", "#saveteam", function(){
+		$("#responseform").hide().parent().parent().removeClass();
 		event.preventDefault();
 		console.log("save team");
 		// tenter d'ajouter la team dans la base
@@ -10,13 +12,22 @@ $(function(){
 			function(result){
 				console.log("done");
 				console.log(result);
-			}
+			},
+			"json"
 		).fail(function(erreur){
 			console.log("fail");
-			console.log(erreur);
+			$("#responseform").addClass("bg-danger text-light").html(erreur.msg).show();
 		}).always(function(res){
 			console.log("always");
 			console.log(res);
+			blou = res;
+			if (!res.save){
+				$("#responseform").html(res.msg).show("fast", "swing").parent().parent().addClass("bg-warning text-light");
+			} else if (res.erreur) {
+				$("#responseform").html(res.msg).show("fast", "swing").parent().parent().addClass("bg-danger text-light");
+			} else {
+				$("#responseform").html(res.msg).show("fast", "swing").parent().parent().addClass("bg-success text-light");
+			}
 		});
 		
 		// traiter la réponse : la team n'est pas ajouté dans la base ----> on reste et on affiche un petit mot
