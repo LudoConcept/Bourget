4afd081bf67def809a4ae5093decc81e50a733d5
Reoddyn <59644898+Reoddyn@users.noreply.github.com>
2022-09-08 17:08:59 +0200

    Update bdd_connect.php

--- action/bdd_connect.php
+++ action/bdd_connect.php
@@ -1,8 +1,16 @@
 <?php
+
+	// dev
 	$servername = "localhost";
 	$username = "bourget";
 	$password = "test";
 	$dbname = "bourget";
+	
+	// prod
+	/* $servername = "acrcugpcity.mysql.db";
+	$username = "acrcugpcity";
+	$password = "Evasi0n73";
+	$dbname = "acrcugpcity"; */
 	
 	$sql = "";
 	$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
