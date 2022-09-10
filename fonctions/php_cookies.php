46b50760ca53f7ad9c83232f1a6b58e174ff4fec
LudoConcept <lmarechal.dev@gmail.com>
2022-09-10 20:37:51 +0200

    ACTIVATION DOMAIN VALIDE POUR LA MISE EN LIGNE

--- fonctions/php_cookies.php
+++ fonctions/php_cookies.php
@@ -1,8 +1,8 @@
 <?php
 // options pour les cookies, la clef expires doit être précisée avant utilisation du tableau
 // define("DOMAIN", "bourget");
-define("DOMAIN", "localbourget");
-// define("DOMAIN", "jeuext.la-grande-evasion.com");
+// define("DOMAIN", "localbourget");
+define("DOMAIN", "jeuext.la-grande-evasion.com");
 
 $options = ['expires' => -1, 'path' => PATH, 'domain' => DOMAIN, 'secure' => COOKIESECURE, 'httponly' => COOKIEHTTPONLY, 'samesite' => 'Lax'];
 
